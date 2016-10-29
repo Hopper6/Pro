bb4541cf30eb2ad26d3c92ea6c904074fa458082
hopper <hoppergoo@gmail.com>
2016-10-29 17:13:54 +0800

    echarts

--- echart/web/js/index.js
+++ echart/web/js/index.js
@@ -2,8 +2,8 @@
  * Created by hopper on 2016/10/28.
  */
 /**
-* 基本操作：如何放置数据
-* */
+ * 基本操作：如何放置数据
+ * */
 // 基于准备好的dom，初始化echarts实例
 var myChart = echarts.init(document.getElementById('main'));
 
@@ -14,10 +14,10 @@ var option = {
     },
     tooltip: {},
     legend: {
-        data:['销量']
+        data: ['销量']
     },
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
     },
     yAxis: {},
     series: [{
@@ -31,36 +31,55 @@ var option = {
 myChart.setOption(option);
 
 /**
-* 样式
-* */
+ * 样式
+ * */
 var pie = echarts.init(document.getElementById('pie'));
 pie.setOption({
-   series: [{
-       name: '访问来源',
-       type: 'pie',
-       data: [
-           {name: '搜索引擎', value: 400},
-           {name: '直接访问', value: 336},
-           {name: '邮件营销', value: 310},
-           {name: '联盟广告', value: 274},
-           {
-               name: '视频广告',
-               value: 236,
-               // selected: true,
-               itemStyle: {
-                   normal: {
-                       color: 'red',
-                       borderColor: 'black',
-                       borderWidth: 1,
-                       borderType: 'dotted',
-                       shadowColor: 'rgba(128, 0, 0, 0.5)',
-                       shadowBlur: 10
-                   },
-                   emphasis: {
-                       color: 'rgba(128, 128, 128, 0.5)'
-                   }
-               }
-           }
-       ]
-   }]
+    backgroundColor: '#2c343c',
+    textStyle: {
+        color: 'rgba(255,255,255,0.3)'
+    },
+    legend: {
+        textStyle: {
+            color: 'gray'
+        },
+        data: ['搜索引擎','直接访问','邮件营销','联盟广告'],
+        x: 'left',
+        orient: 'vertical'
+    },
+    series: [{
+        name: '访问来源',
+        type: 'pie',
+        data: [
+            {name: '搜索引擎', value: 400},
+            {name: '直接访问', value: 336},
+            {name: '邮件营销', value: 310},
+            {name: '联盟广告', value: 274},
+            {
+                name: '视频广告',
+                value: 236,
+                // selected: true,
+                itemStyle: {
+                    normal: {
+                        color: 'red',
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderType: 'dotted',
+                        shadowColor: 'rgba(128, 0, 0, 0.5)',
+                        shadowBlur: 10
+                    },
+                    emphasis: {
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    }
+                }
+            }
+        ],
+        label: {
+            normal: {
+                position: 'outside',
+                // show: false
+            }
+        },
+        roseType: 'angle'
+    }]
 });
