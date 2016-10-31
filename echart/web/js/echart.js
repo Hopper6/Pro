b10358f874db86664b6cb2bda58f2ed9f36663ab
hopper <hoppergoo@gmail.com>
2016-10-31 17:48:21 +0800

    echarts

---
+++ echart/web/js/echart.js
@@ -0,0 +1,140 @@
+/**
+ * Created by hopper on 2016/10/31.
+ */
+
+/**
+ * 现场检查统计
+ * */
+var eStat = echarts.init(document.getElementById('stat'));
+eStat.setOption({
+    // title: {
+    //     text: '现场检查统计',
+    //     subtext: '数据虚构',
+    //     textStyle: {
+    //         color: '#00DEFF',
+    //         fontFamily: '微软雅黑'
+    //     }
+    // },
+    textStyle: {
+        color: '#00DEFF',
+        fontFamily: '微软雅黑'
+    },
+    legend: {
+        data: ['检查人次', 'WHS检查数', '安全文明检查数'],
+        textStyle: {
+            color: '#00DEFF',
+            fontFamily: '微软雅黑'
+        }
+    },
+    xAxis: {
+        type: 'category',
+        data: ['花都供电局', '海珠供电局', '番禺供电局', '从化供电局', '白云供电局', '增城供电局']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: '检查人次',
+            type: 'bar',
+            data: [10, 20, 30, 40, 50, 60]
+        },
+        {
+            name: 'WHS检查数',
+            type: 'bar',
+            data: [60, 50, 40, 30, 20, 10]
+        },
+        {
+            name: '安全文明检查数',
+            type: 'bar',
+            data: [50, 50, 50, 50, 50, 50]
+        }
+    ]
+});
+/**
+ * 施工到位率统计
+ * */
+var sign = echarts.init(document.getElementById('sign'));
+sign.setOption({
+    // title: {
+    //     text: '施工到位率统计',
+    //     textStyle: {
+    //         color: '#00DEFF',
+    //         fontFamily: '微软雅黑'
+    //     }
+    // },
+    backgroundColor: 'rgba(66,90,119,0.5)',
+    textStyle: {
+        color: '#00DEFF',
+        fontFamily: '微软雅黑'
+    },
+    legend: {
+        data: ['施工签到率', '施工签到率1'],
+        textStyle: {
+            color: '#00deff',
+            fontFamily: '微软雅黑'
+        }
+    },
+    xAxis: {
+        type: 'category',
+        data: ['10月1日', '10月2日', '10月3日', '10月4日', '10月5日', '10月6日'],
+        boundaryGap: false
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: '施工签到率',
+            type: 'line',
+            areaStyle: {normal: {}},
+            data: [10, 30, 50, 70, 100, 160]
+        },
+        {
+            name: '施工签到率1',
+            type: 'line',
+            areaStyle: {normal: {}},
+            data: [150, 120, 100, 90, 50, 10]
+        }
+    ]
+});
+/**
+ *区局进度计划完成率
+ * */
+var eProgress = echarts.init(document.getElementById('progress'));
+eProgress.setOption({
+    // title: {
+    //     text: '区局进度计划完成率',
+    //     subText: '数据虚构',
+    //     x: 'center',
+    //     textStyle: {
+    //         color: '#00DEFF',
+    //         fontFamily: '微软雅黑'
+    //     }
+    // },
+    textStyle: {
+        color: '#00DEFF',
+        fontFamily: '微软雅黑'
+    },
+    legend: {
+        data: ['花都供电局', '海珠供电局', '番禺供电局', '从化供电局', '白云供电局', '增城供电局'],
+        x: 'left',
+        orient: 'vertical',
+        textStyle: {
+            color: '#00DEFF',
+            fontFamily: '微软雅黑'
+        }
+    },
+    series: [{
+        name: '计划完成率',
+        type: 'pie',
+        data: [
+            {name: '花都供电局', value: 20},
+            {name: '海珠供电局', value: 35},
+            {name: '番禺供电局', value: 100},
+            {name: '从化供电局', value: 10},
+            {name: '白云供电局', value: 20},
+            {name: '增城供电局', value: 10}
+        ]
+    }]
+})
